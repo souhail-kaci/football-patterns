3b80d30959d6eb429df32ca2fd82703b917f5064
souhail-kaci <souhail_196@hotmail.com>
2024-05-03 22:03:20 +0200

    init project

---
+++ src/main/java/fr/football/patterns/App.java
@@ -0,0 +1,13 @@
+package fr.football.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
